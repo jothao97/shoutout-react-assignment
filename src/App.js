b128296a76bc5b0e2d21151dfc6c3fd9093c0d43
JoAnn <joann.thao@live.com>
2024-05-08 15:33:47 -0500

    finish app js file

--- src/App.js
+++ src/App.js
@@ -1,38 +1,73 @@
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDatePicker from 'react-datepicker';
-import { useState } from 'react';
+
 
 function App() {
 
-const [date, setDate] = useState(new Date());
-
-const [message, setMessage] = useState('');
-
-const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+  const [username, setUsername] = useState('');
+  const [date, setDate] = useState(new Date());
+  const [shoutouts, setShoutouts] = useState([])
 
 const handleSubmit = (e) => {
   e.preventDefault();
-  onSubmit({date, message, username});
-}
+
+  const newShoutout = {
+    message: message,
+    name: username,
+    date: date
+  };
+
+  // Update shoutouts state with new shoutout
+    setShoutouts([...shoutouts, newShoutout]);
+
+    // Reset form fields
+    setMessage('');
+    setUsername('');
+    setDate(new Date());
+  };
 
   return (
     <div className='app'>
-      <form>
-        <div className='input-group'>
-          <input name='announcement-msg'>{announcementMessage}</input>
+      <h1>Shout out!</h1>
+
+      {/* Display shoutouts */}
+      {shoutouts.map((shoutout, index) => (
+        <div key={index}>
+          <p>Message: {shoutout.message}</p>
+          <p>Name: {shoutout.name}</p>
+          <p>Date: {shoutout.date.toString()}</p>
         </div>
+      ))}
+
+      {/* Form */}
+      <form onSubmit={handleSubmit}>
+
         <div className='input-group'>
-          <input name='username' type='text'>username</input>
+          <input name='message' type='text' onChange={(e) => setMessage} value={message} placeholder='message input'/>
         </div>
+
+        <div className='input-group'>
+          <input name='username' type='text' onChange={(e) => setUsername} value={username} placeholder='name input'/>
+        </div>
+
         <div className='input-group'>
           <input name='datepicker'>
-            <ReactDatePicker selected={date} onChange={(date) => setDate(date)}></ReactDatePicker>
+            <ReactDatePicker selected={date} onChange={(date) => setDate(date)} value={date}/>
           </input>
+
         </div>
+
+        <div>
+          <button type='submit'>Send Shoutout</button>
+        </div>
+
       </form>
     </div>
   );
 }
 
+
 export default App;
+
